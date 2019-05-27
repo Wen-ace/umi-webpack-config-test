8cdd7e26b3465c9533ebedb67f02e1be1dabb8e5
wen-ace <local@local.com>
2019-05-27 11:59:59 +0800

    add

---
+++ .umirc.js
@@ -0,0 +1,35 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: false,
+      dva: false,
+      dynamicImport: false,
+      title: 'react-static-url-test',
+      dll: false,
+
+      routes: {
+        exclude: [
+          /components\//,
+        ],
+      },
+    }],
+  ],
+  // runtimePublicPath: true,
+  // exportStatic: {
+  //   dynamicRoot: 'app'
+  // },
+  publicPath: 'http://localhost:8000/',                                         // 配置 静态资源路径  css js img 等等
+  urlLoaderExcludes: [/\.(png|jpe?g|gif|svg)$/],                                // 不使用webpack图片的默认配置
+  chainWebpack: (config, { webpack }) => {                                      // 重新配置webpack规则 rule ,这里以修改图片file-loader为例（取消小图片转换base64配置）。
+    config.module.rule('assets')
+      .test(/\.(png|jpe?g|gif|svg)$/)
+      .use('file')
+      .loader('file-loader')
+      .options({ limit: 0 })
+      ;
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import styles from './index.css';
+
+import tk from '../assets/imgs/ticket_bg.png';
+console.log( tk);
+
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <li>
+          <a href="https://umijs.org/guide/getting-started.html">
+            Getting Started
+          </a>
+        </li>
+      </ul>
+
+      <img src={tk} />
+    </div>
+  );
+}
